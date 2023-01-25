aeb6600bc965ae179150dad624b2f5999cbedb0d
AashishVerma07 <aashishverma0706@gmail.com>
2023-01-25 23:34:53 +0530

    Complete Length section

--- Unit-conversion-application/src/Length.java
+++ Unit-conversion-application/src/Length.java
@@ -10,40 +10,112 @@ public class Length {
                 + "5 : Micrometer\n");
         System.out.print("Select from unit : ");
         int typeFrom = sc.nextInt();
-        System.out.println("Enter the number : ");
-        Float number = sc.nextFloat();
-        if(typeFrom == 1){
-            double meterToKM = typeFrom % 1000;
-            double meterToCM = typeFrom * 100;
-            double meterToMM = typeFrom * 1000;
-            double meterToMC = typeFrom * 1000000;
-            System.out.println("All Possible units : \n" + "Meter to Km - "+meterToKM +"\n"+  "Meter to CM - "+meterToCM +"\n"+ "Meter to MM - "+meterToMM +"\n"+ "Meter to MC - "+ meterToMC);
-        }else if(typeFrom == 2){
-            double KMToM = typeFrom * 1000;
-            double KMToCM = typeFrom * 100000;
-            double KMToMM = typeFrom * 1000000;
-            double KMToMC = typeFrom * 1000000000;
-            System.out.println("All Possible units : \n" + "KM to M - "+KMToM +"\n"+  "KM to CM - "+KMToCM +"\n"+ "KM to MM - "+KMToMM +"\n"+ "KM to MC - "+ KMToMC);
-        }else if(typeFrom == 3){
-            double CMToM = typeFrom * 0.01;
-            double CMToKM = typeFrom * 0.00001;
-            double CMToMM = typeFrom * 10;
-            double CMToMC = typeFrom * 10000;
-            System.out.println("All Possible units : \n" + "CM to M - "+CMToM +"\n"+  "CM to KM - "+ CMToKM +"\n"+ "CM to MM - "+CMToMM +"\n"+ "CM to MC - "+ CMToMC);
-        }else if(typeFrom == 4){
-            double MMToM = typeFrom * 0.001;
-            double MMToKM = typeFrom % 1000000;
-            double MMToCM = typeFrom % 10;
-            double MMToMC = typeFrom * 1000;
-            System.out.println("All Possible units : \n" + "MM to M - "+MMToM +"\n"+  "MM to KM - "+ MMToKM +"\n"+ "MM to CM - "+MMToCM +"\n"+ "MM to MC - "+ MMToMC);
+        System.out.print("Enter the number : ");
+        long number = sc.nextLong();
 
-        }else if(typeFrom == 5){
-            double MCToM = typeFrom % 1000000;
-            double MCToKM = typeFrom % 1000000000;
-            double MCToCM = typeFrom % 10000;
-            double MCToMM = typeFrom % 1000;
-            System.out.println("All Possible units : \n" + "MC to M - "+MCToM +"\n"+  "MC to KM - "+ MCToKM +"\n"+ "MC to CM - "+MCToCM +"\n"+ "MC to MM - "+ MCToMM);
+        if (typeFrom == 1) {
+            System.out.print("Select to unit : ");
+            long typeTo = sc.nextLong();
 
+            if (typeTo == 1) {
+                System.out.printf("Meter to Meter : %.10f" , number);
+            } else if (typeTo == 2) {
+                double meterToKM = number * 0.001;
+                System.out.printf("Meter to KM : %.5f",  meterToKM);
+            } else if (typeTo == 3) {
+                double meterToCM = number * 100;
+                System.out.printf("Meter to CM : %.10f" , meterToCM);
+            } else if (typeTo == 4) {
+                double meterToMM = number * 1000;
+                System.out.printf("Meter to MM : %.10f" , meterToMM);
+            } else if (typeTo == 5) {
+                double meterToMC = number * 1000000;
+                System.out.printf("Meter to MC : %.10f" , meterToMC);
+            }else{
+                System.out.println("Invalid Option!!!");
+            }
+        } else if (typeFrom == 2) {
+            System.out.print("Select to unit : ");
+            long typeTo = sc.nextLong();
+            if (typeTo == 1) {
+                double KMToM = number * 1000;
+                System.out.printf("KM to Meter : %.10f" , KMToM);
+            } else if (typeTo == 2) {
+                System.out.printf("KM to KM : %.10f " , number);
+            } else if (typeTo == 3) {
+                double KMToCM = number * 100000;
+                System.out.printf("KM to CM : %.10f" , KMToCM);
+            } else if (typeTo == 4) {
+                double KMToMM = number * 1000000;
+                System.out.printf("KM to MM : %.10f" , KMToMM);
+            } else if (typeTo == 5) {
+                double KMToMC = number * 1000000000;
+                System.out.printf("KM to MC : %.10f" , KMToMC);
+            }else{
+                System.out.println("Invalid Option!!!");
+            }
+        } else if (typeFrom == 3) {
+            System.out.print("Select to unit : ");
+            long typeTo = sc.nextLong();
+            if (typeTo == 1) {
+                double CMToM = number * 0.01;
+                System.out.printf("CM to Meter : %.10f" , CMToM);
+            } else if (typeTo == 2) {
+                double CMToKM = number * 0.00001;
+                System.out.printf("CM to KM : %.10f" , CMToKM);
+            } else if (typeTo == 3) {
+                System.out.printf("CM to CM : %.10f" , number);
+            } else if (typeTo == 4) {
+                double CMToMM = number * 10;
+                System.out.printf("CM to MM : %.10f" , CMToMM);
+            } else if (typeTo == 5) {
+                double CMToMC = number * 10000;
+                System.out.printf("CM to MC : %.10f" , CMToMC);
+            }else{
+                System.out.println("Invalid Option!!!");
+            }
+        } else if (typeFrom == 4) {
+            System.out.print("Select to unit : ");
+            long typeTo = sc.nextLong();
+            if (typeTo == 1) {
+                double MMToM = number * 1000;
+                System.out.printf("MM to Meter : %.10f" , MMToM);
+            } else if (typeTo == 2) {
+                double MMToKM = number * 0.000001;
+                System.out.printf("MM to KM : %.10f" , MMToKM);
+            } else if (typeTo == 3) {
+                double MMToCM = number * 0.1;
+                System.out.printf("MM to CM : %.10f" , MMToCM);
+            } else if (typeTo == 4) {
+                System.out.printf("MM to MM : %.10f" , number);
+            } else if (typeTo == 5) {
+                double MMToMC = number * 1000;
+                System.out.printf("MM to MC : %.10f" , MMToMC);
+            }else{
+                System.out.println("Invalid Option!!!");
+            }
+        } else if (typeFrom == 5) {
+            System.out.print("Select to unit : ");
+            long typeTo = sc.nextLong();
+            if (typeTo == 1) {
+                double MCToM = (number * 0.000001 );
+                System.out.printf("MC to Meter : %.10f", MCToM);
+            } else if (typeTo == 2) {
+                double MCToKM = number * 0.000000001;
+                System.out.printf( "MC to KM : %.10f" , MCToKM);
+            } else if (typeTo == 3) {
+                double MCToCM = number * 0.0001;
+                System.out.printf("MC to CM : %.10f" , MCToCM);
+            } else if (typeTo == 4) {
+                double MCToMM = number * 0.001;
+                System.out.printf("MC to MM : %.10f" , MCToMM);
+            } else if (typeTo == 5) {
+                System.out.printf("MC to MC : %.10f" , number);
+            }else{
+                System.out.println("Invalid Option!!!");
+            }
+        }else{
+            System.out.println("Invalid Option!!!");
         }
 
     }
--- Unit-conversion-application/src/Main.java
+++ Unit-conversion-application/src/Main.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Length l = new Length();
         Height h = new Height();
         Temperature t = new Temperature();
@@ -11,22 +12,26 @@ public class Main {
         System.out.print("1 : Length\n"
                 + "2 : Height\n"
                 + "3 : Temperature\n"
-                + "4 : Volune\n");
+                + "4 : Volume\n");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Choose the unit : ");
+        System.out.print("Choose the unit : ");
         int unit = scanner.nextInt();
 
         if( unit == 1){
+            System.out.println("Length");
             l.unitsPerTypes();
         }
         else if( unit == 2){
+            System.out.println("Height");
 
         }
         else if (unit == 3) {
+            System.out.println("Temperature");
 
         }
         else if (unit ==4) {
+            System.out.println("Volume");
 
         }else{
             System.out.println("Invalid Option!");
